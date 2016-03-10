e4dc3f52e43524e10211edc04db527318677eff2
sblyman <sblyman@users.noreply.github.com>
2016-03-10 11:51:41 -0500

    Fix issue where the MedDRA hierarchy included in the concept_path for a SNOMED concept was not correct
    In at least 50% of SNOMED concepts included in the condition_treemap.json file, the associated MedDRA hierarchy (PT, HLT, HLGT, SOC) was not correct.  This fix restructures the hierarchy building logic to consistency get the correct MedDRA hierarchy for associated with a SNOMED concept.

--- inst/sql/sql_server/export_v5/condition/sqlConditionTreemap.sql
+++ inst/sql/sql_server/export_v5/condition/sqlConditionTreemap.sql
@@ -1,5 +1,5 @@
 select   concept_hierarchy.concept_id,
-  isNull(concept_hierarchy.soc_concept_name,'NA') + '||' + isNull(concept_hierarchy.hlgt_concept_name,'NA') + '||' + isNull(concept_hierarchy.hlt_concept_name,'NA') + '||' + isNull(concept_hierarchy.pt_concept_name,'NA') + '||' + isNull(concept_hierarchy.snomed_concept_name,'NA') concept_path,	ar1.count_value as num_persons, 
+  isNull(concept_hierarchy.soc_concept_name,'NA') + '||' + isNull(concept_hierarchy.hlgt_concept_name,'NA') + '||' + isNull(concept_hierarchy.hlt_concept_name,'NA') + '||' + isNull(concept_hierarchy.pt_concept_name,'NA') + '||' + isNull(concept_hierarchy.snomed_concept_name,'NA') concept_path,	ar1.count_value as num_persons,
 	round(1.0*ar1.count_value / denom.count_value,5) as percent_persons,
 	round(1.0*ar2.count_value / ar1.count_value,5) as records_per_person
 from (select * from @results_database_schema.ACHILLES_results where analysis_id = 400) ar1
@@ -8,32 +8,39 @@ from (select * from @results_database_schema.ACHILLES_results where analysis_id 
 	on ar1.stratum_1 = ar2.stratum_1
 	inner join
 	(
-		select snomed.concept_id, 
+		select snomed.concept_id,
 			snomed.concept_name as snomed_concept_name,
 			pt_to_hlt.pt_concept_name,
 			hlt_to_hlgt.hlt_concept_name,
 			hlgt_to_soc.hlgt_concept_name,
 			soc.concept_name as soc_concept_name
-		from	
+		from
 		(
 			select concept_id, concept_name
 			from @cdm_database_schema.concept
 			where domain_id = 'Condition'
+			and vocabulary_id = 'SNOMED'
+			and invalid_reason is null
 		) snomed
 		left join
-			(select c1.concept_id as snomed_concept_id, max(c2.concept_id) as pt_concept_id
+			(
+			select inner_snomed_to_pt.c1_concept_id as snomed_concept_id,
+				inner_snomed_to_pt.c2_concept_id as pt_concept_id
 			from
-			@cdm_database_schema.concept c1
-			inner join 
-			@cdm_database_schema.concept_ancestor ca1
-			on c1.concept_id = ca1.descendant_concept_id
-			and c1.domain_id = 'Condition'
-			and ca1.min_levels_of_separation = 1
-			inner join 
-			@cdm_database_schema.concept c2
-			on ca1.ancestor_concept_id = c2.concept_id
-			and c2.vocabulary_id = 'MedDRA'
-			group by c1.concept_id
+				(
+					select c1.concept_id as c1_concept_id,
+						c2.concept_id as c2_concept_id,
+						row_number() over(partition by c1.concept_id order by ca1.min_levels_of_separation, c2.concept_id desc) as row_num  -- get the lowest min sep and the highest concept_id (if there are multiple PT matches)
+					from @cdm_database_schema.concept c1
+					inner join @cdm_database_schema.concept_ancestor ca1 on c1.concept_id = ca1.descendant_concept_id
+  					and vocabulary_id = 'SNOMED'
+	  				and c1.domain_id = 'Condition'
+					inner join @cdm_database_schema.concept c2 on ca1.ancestor_concept_id = c2.concept_id
+		  			and c2.vocabulary_id = 'MedDRA'
+			  		and c2.concept_class_id = 'PT'
+				  	and c2.domain_id = 'Condition'
+				) inner_snomed_to_pt
+				where row_num = 1
 			) snomed_to_pt
 		on snomed.concept_id = snomed_to_pt.snomed_concept_id
 
@@ -41,15 +48,17 @@ from (select * from @results_database_schema.ACHILLES_results where analysis_id 
 			(select c1.concept_id as pt_concept_id, c1.concept_name as pt_concept_name, max(c2.concept_id) as hlt_concept_id
 			from
 			@cdm_database_schema.concept c1
-			inner join 
+			inner join
 			@cdm_database_schema.concept_ancestor ca1
 			on c1.concept_id = ca1.descendant_concept_id
-			and c1.vocabulary_id = 'MedDRA'
-			and ca1.min_levels_of_separation = 1
-			inner join 
+			  and c1.vocabulary_id = 'MedDRA'
+			  and c1.concept_class_id = 'PT'
+			  and ca1.min_levels_of_separation = 1
+			inner join
 		  @cdm_database_schema.concept c2
 			on ca1.ancestor_concept_id = c2.concept_id
-			and c2.vocabulary_id = 'MedDRA'
+			  and c2.vocabulary_id = 'MedDRA'
+			  and c2.concept_class_id = 'HLT'
 			group by c1.concept_id, c1.concept_name
 			) pt_to_hlt
 		on snomed_to_pt.pt_concept_id = pt_to_hlt.pt_concept_id
@@ -58,15 +67,17 @@ from (select * from @results_database_schema.ACHILLES_results where analysis_id 
 			(select c1.concept_id as hlt_concept_id, c1.concept_name as hlt_concept_name, max(c2.concept_id) as hlgt_concept_id
 			from
 			@cdm_database_schema.concept c1
-			inner join 
+			inner join
 			@cdm_database_schema.concept_ancestor ca1
 			on c1.concept_id = ca1.descendant_concept_id
-			and c1.vocabulary_id = 'MedDRA'
-			and ca1.min_levels_of_separation = 1
-			inner join 
+			  and c1.vocabulary_id = 'MedDRA'
+			  and c1.concept_class_id = 'HLT'
+			  and ca1.min_levels_of_separation = 1
+			inner join
 			@cdm_database_schema.concept c2
 			on ca1.ancestor_concept_id = c2.concept_id
-			and c2.vocabulary_id = 'MedDRA'
+			  and c2.vocabulary_id = 'MedDRA'
+			  and c2.concept_class_id = 'HLGT'
 			group by c1.concept_id, c1.concept_name
 			) hlt_to_hlgt
 		on pt_to_hlt.hlt_concept_id = hlt_to_hlgt.hlt_concept_id
@@ -75,21 +86,24 @@ from (select * from @results_database_schema.ACHILLES_results where analysis_id 
 			(select c1.concept_id as hlgt_concept_id, c1.concept_name as hlgt_concept_name, max(c2.concept_id) as soc_concept_id
 			from
 			@cdm_database_schema.concept c1
-			inner join 
+			inner join
 			@cdm_database_schema.concept_ancestor ca1
 			on c1.concept_id = ca1.descendant_concept_id
-			and c1.vocabulary_id = 'MedDRA'
-			and ca1.min_levels_of_separation = 1
-			inner join 
+			  and c1.vocabulary_id = 'MedDRA'
+			  and c1.concept_class_id = 'HLGT'
+			  and ca1.min_levels_of_separation = 1
+			inner join
 			@cdm_database_schema.concept c2
 			on ca1.ancestor_concept_id = c2.concept_id
-			and c2.vocabulary_id = 'MedDRA'
+			  and c2.vocabulary_id = 'MedDRA'
+			  and c2.concept_class_id = 'SOC'
 			group by c1.concept_id, c1.concept_name
 			) hlgt_to_soc
 		on hlt_to_hlgt.hlgt_concept_id = hlgt_to_soc.hlgt_concept_id
 
 		left join @cdm_database_schema.concept soc
 		 on hlgt_to_soc.soc_concept_id = soc.concept_id
+
 	) concept_hierarchy
 	on ar1.stratum_1 = CAST(concept_hierarchy.concept_id as VARCHAR)
 	,
